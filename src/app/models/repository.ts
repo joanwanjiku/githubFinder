4ac3a507c7a6fef0c2cf9cbdd4fbd29ccded2504
Joan_E <joanevans18@gmail.com>
2020-06-19 23:27:29 +0300

    Added two classes user and repository

--- src/app/models/repository.ts
+++ src/app/models/repository.ts
@@ -1,2 +1,7 @@
 export class Repository {
+    constructor(
+        public name: string,
+        public description: string,
+        public createdDate: Date    
+    ){}
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,10 @@
 export class User {
+    constructor(
+        public name: string,
+        public avatar: string,
+        public numberOfRepos: number,
+        public bio: string,
+        public location: string,
+        public memberSince: Date
+    ){}
 }
