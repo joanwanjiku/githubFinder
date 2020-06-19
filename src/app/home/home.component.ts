4629aa0d99199d06ca5426a4dc6036afe45ecbe0
Joan_E <joanevans18@gmail.com>
2020-06-19 23:30:05 +0300

    Displayed github profile for the user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../models/user'
+import {UserService} from '../services/user.service'
+import {RepositoryService} from '../services/repository.service'
+import {Repository} from '../models/repository'
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  currentUser: User
+  repos: Repository[]
 
-  constructor() { }
+  constructor(private userService: UserService, private repoService: RepositoryService) { }
 
   ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe({
+      // user => this.currentUser = user,
+      next: user => this.currentUser = user
+      // next(user) {console.log(user)}
+    })
+    this.repoService.getUserRepos().subscribe({
+      next: repos => this.repos = repos
+      // next(repos) {console.log(repos)}
+    })
   }
 
 }
