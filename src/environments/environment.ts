b7b58c4d274f8bf415f9270115d60bcf463a0f2b
Joan_E <joanevans18@gmail.com>
2020-06-19 23:28:55 +0300

    Added urls to environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  userUrl: 'https://api.github.com/users/joanwanjiku',
+  reposUrl: 'https://api.github.com/users/joanwanjiku/repos'
 };
 
 /*
