048d6230151fd0e34e61fd42e380f3c7435c1506
Joan_E <joanevans18@gmail.com>
2020-06-19 23:31:46 +0300

    Added httpClientModule

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../models/user'
+import {UserService} from '../services/user.service'
 
 @Component({
   selector: 'app-user',
@@ -10,6 +12,7 @@ export class UserComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
