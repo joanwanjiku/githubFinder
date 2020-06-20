c9b37cbe4ef41ae71f4ab882fdb0cc57193d4feb
Joan_E <joanevans18@gmail.com>
2020-06-20 12:12:02 +0300

    displayed the searched github user profile

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../models/user'
 import {UserService} from '../services/user.service'
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-user',
@@ -8,8 +10,20 @@ import {UserService} from '../services/user.service'
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  userName: User
+  retrievedUser: User
+  constructor(private http: HttpClient) {
+    this.userName = new User("", "",0, "", "", new Date)
+   }
 
-  constructor() { }
+  submitUserName(){
+     console.log(this.userName['name'])
+     this.http.get<User>(`https://api.github.com/users/${this.userName['name']}`).subscribe({
+      //  next(response) {console.log(response)}
+      next: user => this.retrievedUser = user
+     })
+   }
+
 
   ngOnInit(): void {
     
